0308736e6018da404803d55561da57bc98f25b1b
ma1318k <ma1318kimura@gmail.com>
2022-06-07 13:26:51 +0900

    git reset --hard

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--n3%+2zpmi0zc^pyv@=ui41zzm42jkbt6=*e&+*+xkx*gcr#&1'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
