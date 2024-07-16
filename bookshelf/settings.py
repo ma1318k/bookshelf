31c44eee6bf4f18530906b271ea3554fb81f8961
ma1318k <ma1318kimura@gmail.com>
2024-07-16 18:04:31 +0900

    MOD：Pythonanywhere環境対応

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'django-bookshelf.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-bookshelf.herokuapp.com', '.pythonanywhere.com', 'ma1318.pythonanywhere.com']
 
 
 # Application definition
@@ -159,6 +160,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = False
